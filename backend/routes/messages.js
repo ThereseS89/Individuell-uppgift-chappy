47e49b30ae6972dc423ef01791d5969441c28d8a
Therese <thereseskold@live.com>
2023-06-07 11:18:28 +0200

    made the GET messages

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,11 +1,20 @@
 import express from "express"
+import { getDb } from "../data/database.js"
 
 const router = express.Router()
 
+const db = getDb()
 // Här ska alla metoder som behövs komma in
 
+// GET
+router.get('/', async (req, res) => {
+	await db.read()
+	let messages = db.data.messages
+
+	res.send(messages)
+})
 //POST
 //DELETE
-//GET
+
 
 export default router
