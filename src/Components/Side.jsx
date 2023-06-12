9e65028bf189fed70e7f279db351978cfded56c3
Therese <thereseskold@live.com>
2023-06-12 14:29:02 +0200

    making the channels locked

--- src/Components/Side.jsx
+++ src/Components/Side.jsx
@@ -39,7 +39,7 @@ const [channels, setChannels] = useState([])
 				<div
 				className="channels-container"
 				key={channels.id}>
-					<li>{channel.name}</li>
+					 <li onClick={() => {handleAccessToChannel(channel.id)}}>{channel.name}</li>
 				</div>
 
 			)
@@ -47,7 +47,7 @@ const [channels, setChannels] = useState([])
 			)}
 			<li> [Kanaler] </li>
 			<li><NavLink to="/koda"> #koda </NavLink></li>
-			<li><p onClick={() => {handleAccessToChannel()}}> #random </p></li>
+			<li>}> #random </p></li>
 			<p>{errorMessage}</p>
 			<li className="locked"><NavLink to="/gruppEtt"> #grupp1 🔒 </NavLink></li>
 			<li className="selected"><NavLink to="/gruppTvå"> #grupp2 🔑 </NavLink></li>
